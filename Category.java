195a3fadec5660450ffcbe6d2d4496c0bb092346
shilfihabibah <146401507+shilfihabibah@users.noreply.github.com>
2024-12-28 15:48:58 +0700

    Category.java

--- Category.java
+++ Category.java
@@ -11,12 +11,32 @@ package com.mycompany.tugas_besar;
 class Category {
     private String categoryId;
     private String name;
+    private List<Product> products;
+
+    public Category(String categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.products = new ArrayList<>();
+    }
 
     public void addProduct(Product product) {
+        products.add(product);
         System.out.println("Product added to category: " + name);
     }
 
     public void removeProduct(Product product) {
+        products.remove(product);
         System.out.println("Product removed from category: " + name);
+    }
+
+    public void displayProducts() {
+        System.out.println("Category: " + name);
+        for (Product product : products) {
+            System.out.println("- " + product.name);
+        }
+    }
+
+    public String getName() {
+        return name;
     }
 }
